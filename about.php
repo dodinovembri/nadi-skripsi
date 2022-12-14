b3d8823c84872175d542a8391e391c5b3254cdda
dodinovembri <dodinovembri32gmail.com>
2022-12-14 11:21:40 +0700

    update

---
+++ about.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en-us">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Skripsi Program | Nadi Skripsi</title>
+    <meta name="author" content="dodinovembri">
+    <meta name="description" content="Jasa Pembuatan Skripsi Program">
+    <meta name="keywords" content="Decision Support System, Knowledge Management System, Data Mining System, Customer Relationship Management, Ecommerce System, Material Requirement Planning" />
+    <link href='https://fonts.googleapis.com/css?family=Source+Sans+Pro:100,300,400,600,700,900,400italic|Montserrat:400,700' rel='stylesheet'>
+    <link href="./assets/css/bootstrap.min.css" rel="stylesheet">
+    <link href="./assets/css/font-awesome.min.css" rel="stylesheet">
+    <link href="./assets/css/style.css" rel="stylesheet">
+</head>
+
+<body>
+    <div id="preloader">
+        <div id="status">&nbsp;</div>
+    </div>
+    <!-- Global Wrapper -->
+    <div id="wrapper">
+        <?php include('components/header.php') ?>
+        <section class="section page-contact mt30 mb30">
+            <div class="container">
+                <div class="row">
+                    <div class="col-sm-12 col-md-7 sm-box3">
+                        <div class="row col-p0">
+                            <div class="col-sm-12">
+                                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3966.536792911367!2d106.76154811430995!3d-6.192673962393819!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69f71bec3fd2d9%3A0xf7ae320e9d457a91!2sJl.%20Melati%20No.21C%2C%20RT.13%2FRW.10%2C%20Kb.%20Jeruk%2C%20Kec.%20Kb.%20Jeruk%2C%20Kota%20Jakarta%20Barat%2C%20Daerah%20Khusus%20Ibukota%20Jakarta%2011530!5e0!3m2!1sen!2sid!4v1670914865971!5m2!1sen!2sid" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-12 col-md-5">
+                        <h3 class="title-small br-bottom">Contact us</h3>
+                        <p class="mb40">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veniam quasi modi delectus aliquid doloribus. Accusantium iste earum saepe provident sapiente fugit, vel perspiciatis harum, tempore id, porro sequi aliquid.</p>
+                        <h3 class="title-small br-bottom">Get social</h3>
+                        <div class="row">
+                            <div class="col-sm-5 col-md-7">
+                                <ul class="sidebar-socials">
+                                    <li><a href="#"><i class="fa fa-twitter"></i> Follow us</a></li>
+                                    <li><a href="#"><i class="fa fa-facebook"></i> Like us</a></li>
+                                    <li><a href="#"><i class="fa fa-envelope"></i> Subscribe</a></li>
+                                    <li><a href="https://dodinovembri.my.id" target="_blank"><i class="fa fa-globe"></i> My Website Profile</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section class="page-contact">
+
+        </section>
+        <?php include('components/footer.php') ?>
+    </div> <!-- END Global Wrapper -->
+    <script src="./assets/js/jquery.min.js"></script>
+    <script src="./assets/js/bootstrap.min.js"></script>
+    <script src="./assets/js/script.js"></script>
+</body>
+
+</html>
