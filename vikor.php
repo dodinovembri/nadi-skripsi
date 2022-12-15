f65e73ce6fe694674b7c0cb3f0f352efc703c6b6
dodinovembri <dodinovembri32gmail.com>
2022-12-15 11:40:15 +0700

    update

---
+++ vikor.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en-us">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Skripsi Program | Nadi Skripsi</title>
+    <meta name="author" content="dodinovembri">
+    <meta name="description" content="Jasa Pembuatan Skripsi Program">
+    <meta name="keywords" content="Decision Support System, Knowledge Management System, Data Mining System, Customer Relationship Management, Ecommerce System, Material Requirement Planning" />
+    <link href='https://fonts.googleapis.com/css?family=Source+Sans+Pro:100,300,400,600,700,900,400italic|Montserrat:400,700' rel='stylesheet'>
+    <link href="./assets/css/bootstrap.min.css" rel="stylesheet">
+    <link href="./assets/css/font-awesome.min.css" rel="stylesheet">
+    <link href="./assets/css/style.css" rel="stylesheet">
+</head>
+
+<body>
+    <div id="preloader">
+        <div id="status">&nbsp;</div>
+    </div>
+    <!-- Global Wrapper -->
+    <div id="wrapper">
+        <?php include('components/header.php') ?>
+        <section class="section mt30">
+            <div class="container">
+                <div class="row">
+                    <div class="col-sm-12">
+                        <div class="row">
+                            <div class="col-sm-12">
+                                <article class="blog-post-content">
+                                    <br><br><br><br><i>Coming Soon..</i><br><br><br><br><br><br>
+                                </article>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <?php include('components/footer.php') ?>
+    </div> <!-- END Global Wrapper -->
+    <script src="./assets/js/jquery.min.js"></script>
+    <script src="./assets/js/bootstrap.min.js"></script>
+    <script src="./assets/js/script.js"></script>
+</body>
+
+</html>
