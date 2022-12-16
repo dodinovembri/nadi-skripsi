a842126b660b9b64a1f3253fb7f390849614ad18
dodinovembri <dodinovembri32gmail.com>
2022-12-16 14:00:18 +0700

    update

--- method.php
+++ method.php
@@ -79,9 +79,14 @@
                                     <li><a href="bc-ratio">SPK - B/C Ratio</a></li>
                                     <li><a href="irr">SPK - Internal Rate of Return (IRR)</a></li>
                                     <li><a href="npv">SPK - Net Present Value (NPV)</a></li>
-                                    <li><a href="fmadm">SPK - Fuzzy Multiple Atribut Decision Making (FMADM)</a></li>   
+                                    <li><a href="fmadm">SPK - Fuzzy Multiple Atribut Decision Making (FMADM)</a></li>
                                     <li><a href="fmadm">SPK - Metode Simple Atribute Rating Technique Exploiting Ranks (Smarter)</a></li>
-                                    <li><a href="fmadm">SPK - Algoritma Rivest-Shamir-Adleman (Rsa)</a></li>
+                                    <li><a href="fmadm">SPK - Algoritma Rivest-Shamir-Adleman (RSA)</a></li>
+                                    <li><a href="fmadm">SPK - Composite Performance Index (Cpi)</a></li>
+                                    <li><a href="fmadm">SPK - Fuzzy - Analytical Hierarchy Process (F-Ahp)</a></li>
+                                    <li><a href="fmadm">SPK - Fuzzy SAW</a></li>
+                                    <li><a href="fmadm">SPK - Metode Borda</a></li>
+                                    
                                 </ul>
                                 <br>
                                 <h3 class="title-small br-bottom">Knowledge Management System (KMS)</h3>
@@ -92,7 +97,15 @@
                                     <li><a href="fcbr">KMS - Fuzzy Case Based Reasoning (FCBR)</a></li>
                                     <li><a href="fcbr">KMS - Seci Model</a></li>
                                     <li><a href="fcbr">KMS - Gamification</a></li>
-                                    <li><a href="fcbr">KMS - Becerra Fernandez</a></li>
+                                    <li><a href="fcbr">KMS - Choo Sense Making</a></li>
+                                    <li><a href="fcbr">KMS - Modified K-Nearest Neighbor (Mknn)</a></li>
+                                    <li><a href="fcbr">KMS - After Action Review</a></li>
+                                    <li><a href="fcbr">KMS - Multinomial Naive Bayes</a></li>
+                                    <li><a href="fcbr">KMS - Algoritma Raita</a></li>
+                                    <li><a href="fcbr">KMS - Vector Space Model (Vsm)</a></li>
+                                    <li><a href="fcbr">KMS - Algoritma Apostolico-Giancarlo</a></li>
+                                    <li><a href="fcbr">KMS - Algoritma Knuth Morris Pratt</a></li>
+                                    
                                     
                                 </ul>
                                 <br>
@@ -103,16 +116,90 @@
                                     <li><a href="cbr">DM - Classification In Accurate Fraud Transcation Classification</a></li>
                                     <li><a href="cbr">DM - Neural Network Backpropagation</a></li>
                                     <li><a href="cbr">DM - K-Means Clustering</a></li>
+                                    <li><a href="cbr">DM - Fuzzy C-Means</a></li>
+                                    <li><a href="cbr">DM - Hybrid Fuzzy C-Means Particle Swarm Optimization</a></li>
+                                    <li><a href="cbr">DM - C4.5</a></li>
+                                    <li><a href="cbr">DM - Association Rule</a></li>
+                                    <li><a href="cbr">DM - Algoritma Genetika</a></li>
+                                    <li><a href="cbr">DM - Fuzzy Time Series Chen</a></li>
+                                    <li><a href="cbr">DM - Classification Based On Association (Cba)</a></li>
+                                    <li><a href="cbr">DM - Decision Tree</a></li>
+                                    <li><a href="cbr">DM - Algoritma Divisive</a></li>
+                                    <li><a href="cbr">DM - Algoritma Fp-Growth</a></li>
+                                    <li><a href="cbr">DM - Trend Least Square</a></li>
+                                    <li><a href="cbr">DM - Metode Harris Corner</a></li>
+                                    <li><a href="cbr">DM - Algoritma Apriori</a></li>
+                                     
+                                    
                                 </ul>
                                 <h3 class="title-small br-bottom">Customer Relationship Management (CRM)</h3>
                                 <ul>
                                     <li><a href="boyer-moore">CRM - Metode LibQual</a></li>
                                     <li><a href="string-matching">CRM - Service Quality</a></li>
                                     <li><a href="cbr">CRM - SMS Gateway</a></li>
+                                    <li><a href="cbr">CRM - End-User Computing Satisfaction (Eucs)</a></li>
+                                    <li><a href="cbr">CRM - Net Promoter Score (Nps)</a></li>
+                                    
                                 </ul>
                                 <h3 class="title-small br-bottom">Supply Chain Management (SCM)</h3>
                                 <ul>
                                     <li><a href="boyer-moore">CRM - Supply Chain Operations Reference (Scor)</a></li>
+                                    <li><a href="boyer-moore">CRM - Economic Order Quantity (EOQ)</a></li>
+                                </ul>
+                                <h3 class="title-small br-bottom">Another Methods</h3>
+                                <ul>
+                                    <li><a href="another-model">E-Democracy</a></li>
+                                    <li><a href="another-model">Utaut(Unified Theory Of Acceptance And Use Of Technology)</a></li>
+                                    <li><a href="another-model">Lexicon-Based</a></li>
+                                    <li><a href="another-model">Technology Acceptance Model (Tam)</a></li>
+                                    <li><a href="another-model">Cobit 5 Domain Mea (Monitor, Evaluate And Assess)</a></li>
+                                    <li><a href="another-model">Cobit 5.0 Dan Itil V3</a></li>
+                                    <li><a href="another-model">Itil V3</a></li>
+                                    <li><a href="another-model">Teknologi Augmented Reality</a></li>
+                                    <li><a href="another-model">RFM</a></li>
+                                    <li><a href="another-model">Business Intelligence</a></li>
+                                    <li><a href="another-model">Framework Of Dynamic Crm</a></li>
+                                    <li><a href="another-model">Metode User Centered Design (Ucd)</a></li>
+                                    <li><a href="another-model">Octalysis Framework</a></li>
+                                    <li><a href="another-model">KMS Becerra Fernandez</a></li>
+                                    <li><a href="another-model">KMS Soft Systems Methodology</a></li>
+                                    <li><a href="another-model">KMS Nusantara Model</a></li>
+                                    <li><a href="another-model">Mass Customization</a></li>
+                                    <li><a href="another-model">Virtual Reality Tour</a></li>
+                                    <li><a href="another-model">Epic Model</a></li>
+                                    <li><a href="another-model">System Usability Scale (Sus)</a></li>
+                                    <li><a href="another-model">Togaf Adm</a></li>
+                                    <li><a href="another-model">Heuristic Evaluation</a></li>
+                                    <li><a href="another-model">Perbandingan Eksponensial</a></li>
+                                    <li><a href="another-model">Algoritma Slope One</a></li>
+                                    <li><a href="another-model">Handheld Augmented Reality Usability Scale (Harus)</a></li>
+                                    <li><a href="another-model">Kualitas Iso/Ies 9126</a></li>
+                                    <li><a href="another-model">Algoritma Cosine Similarity</a></li>
+                                    <li><a href="another-model">Forward Chaining</a></li>
+                                    <li><a href="another-model">Metode Time Series</a></li>
+                                    <li><a href="another-model">Crm-Iris Methodology</a></li>
+                                    <li><a href="another-model">Importance-Performance Analysis</a></li>
+                                    <li><a href="another-model">Process Assessment Model</a></li>
+                                    <li><a href="another-model">Algoritma Dijkstra</a></li>
+                                    <li><a href="another-model">Metode Indexing Field</a></li>
+                                    <li><a href="another-model">Dynamic Systems Development Method (Dsdm)</a></li>
+                                    <li><a href="another-model">Regresi Linear Berganda</a></li>
+                                    <li><a href="another-model">Straight Line</a></li>
+                                    <li><a href="another-model">Learning Vector Quantization </a></li>
+                                    <li><a href="another-model">Continuous Review System (Crs) </a></li>
+                                    <li><a href="another-model">Multifactor Evaluation Process (Mfep) </a></li>
+                                    <li><a href="another-model">Octaveallegro </a></li>
+                                    <li><a href="another-model">Failure Mode And Effect Analysis </a></li>
+                                    <li><a href="another-model">Flocking Algorithm </a></li>
+                                    <li><a href="another-model">Metode Ocatve-S </a></li>
+                                    <li><a href="another-model">Harris Algorithm </a></li>
+                                    <li><a href="another-model">Metode Distribution Requirement Planning (Drp) </a></li>
+                                    <li><a href="another-model">Long Polling </a></li>
+                                    <li><a href="another-model">Content Based Filtering </a></li>
+                                    <li><a href="another-model">Quantitative Strategic Planning Matriks </a></li>
+                                    <li><a href="another-model">Metode Certainty Factor </a></li>
+                                    <li><a href="another-model">Metode Discrete-Event Simulation </a></li>
+                                    
                                 </ul>
                             </div>
                         </div>
